e3c54887ccba052adef4a062e3a88a2ea273f92f
cosformula <cosformula@gmail.com>
2021-07-01 21:38:48 +0800

    feat: favicons

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,6 +41,29 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/icon.svg" />
+        <link rel="mask-icon" href="/mask-icon.svg" color="#002FA7" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
           name="viewport"
           key="viewport"
